6ca4fb8ed6a181fa8b9e37bc039daaea5a0118ef
Jeremy Bryan Ico Herrera <jeremyico78@gmail.com>
2024-06-29 20:11:53 -0600

    agregar logica canvas

---
+++ main.js
@@ -0,0 +1,34 @@
+const canvas = document.querySelector("#game")
+const game = canvas.getContext("2d")
+function setShape(shape){
+    let canvasShape
+    
+    if(window.innerHeight > window.innerWidth) canvasShape = window.innerWidth * .8
+    else canvasShape = window.innerHeight * .8
+    canvas.setAttribute("width",canvasShape)
+    canvas.setAttribute("height",canvasShape)
+    return canvasShape
+}
+window.addEventListener("resize",startGame)
+function startGame(){  
+    const elementSize = setShape() / 10
+
+    game.font = `${elementSize-20}px Verdana`
+    game.textAlign = ""
+    let map = maps[0]
+    map = map.trim().split("\n")
+    map.forEach((line,row) => {
+        console.log(line)
+        line.trim().split("").forEach((caracter,column)=>{
+            console.log(caracter)
+            game.fillText(
+                emojis[caracter],
+                elementSize*(column),
+                elementSize*(row+.5)
+            )
+        })
+    });
+}
+window.addEventListener("load",startGame)
+// detectar movimientos del jugador
+
