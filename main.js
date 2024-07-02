bd26c3129ff4a2d03cdbf1be33cef0b589c0bbea
Jeremy Bryan Ico Herrera <jeremyico78@gmail.com>
2024-07-01 23:22:12 -0600

    logica para siguiente nivel

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector("#game")
 const game = canvas.getContext("2d")
 let canvasShape
+let level = 0
 function getDimensions(){
     if(window.innerHeight > window.innerWidth) return window.innerWidth * .8
     else return window.innerHeight * .8
@@ -11,13 +12,23 @@ function setShape(){
     canvas.setAttribute("height",canvasShape)
     return canvasShape
 }
+function nextLevel(){
+    alert("siguiente nivel")
+    if(maps.length-1 <= level){
+        alert("no hay mas niveles")
+        level = 0
+    }else level++
+    playerPosition.x = undefined
+    playerPosition.y = undefined
+    startGame()
+}
 window.addEventListener("resize",startGame)
 function startGame(){  
     const elementSize = setShape() / 10
     canvasShape = elementSize
     game.font = `${elementSize-20}px Verdana`
     game.textAlign = ""
-    let map = maps[0]
+    let map = maps[level]
     map = map.trim().split("\n")
     game.clearRect(0,0,canvasShape,canvasShape)
     map.forEach((line,row) => {
@@ -34,15 +45,16 @@ function startGame(){
                 playerPosition.y = posY
                 playerPosition.x = posX
             }
-            
+            if(playerPosition.x <= 0) playerPosition.x = 0
             if(
                 parseInt(playerPosition.y) === parseInt(posY) &&
                 parseInt(playerPosition.x) === parseInt(posX) &&
                 caracter !== "-" &&
                 caracter !=="O"
             ){
-                if(caracter === "I") alert("lo hiciste")
-                else alert("1 vida menos por wey")
+                console.log(caracter)
+                if(caracter === "I") nextLevel()
+                else console.log("1 vida menos por wey")
             }
         })
     });
@@ -60,7 +72,7 @@ function showPlayer(){
 }
 function movePlayer(posision,direccion){   
     let move = playerPosition[posision] +(direccion*canvasShape)
-    if(move<-1 || move > getDimensions()) return
+    if(move<-1 || move >= getDimensions()) return 
     playerPosition[posision] = move
     showPlayer()
 }
